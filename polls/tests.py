718b72ca5075458318f9a6ecc433aaee6e5cf81b
carlortiz <mr.carlortiz@gmail.com>
2023-02-21 13:32:09 -0800

    moved executable code out of assertions

--- polls/tests.py
+++ polls/tests.py
@@ -10,15 +10,18 @@ class QuestionModelTestCase(TestCase):
     def test_was_published_recently_with_future_question(self):
         date_tomorrow = timezone.now() + datetime.timedelta(days=1)
         question = Question.objects.create(date_created=date_tomorrow)
-        self.assertEqual(question.was_published_recently(), False)
+        question_published_recently = question.was_published_recently()
+        self.assertEqual(question_published_recently, False)
 
     def test_was_published_recently_with_old_question(self):
         date_over_a_day_ago = timezone.now() - datetime.timedelta(days=1, seconds=1)
         question = Question.objects.create(date_created=date_over_a_day_ago)
-        self.assertEqual(question.was_published_recently(), False)
+        question_published_recently = question.was_published_recently()
+        self.assertEqual(question_published_recently), False)
 
     def test_was_published_recently_with_recent_question(self):
         date_recent = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         question = Question.objects.create(date_created=date_recent)
-        self.assertEqual(question.was_published_recently(), True)
+        question_published_recently = question.was_published_recently()
+        self.assertEqual(question_published_recently, True)
 
